b1c0b76514d310fda8a40306c4a73bdbda7352ee
Nikita Siniayev <ns21@usf.edu>
2025-09-28 14:07:33 -0400

    step 2 done

---
+++ cc_5.js
@@ -0,0 +1,6 @@
+const employees = [
+    {name: 'Alex', hourlyRate: 15, hoursWorked: 25},
+    {name: 'Sam', hourlyRate: 40, hoursWorked: 40},
+    {name: 'Jordan', hourlyRate: 25, hoursWorked: 30}
+];
+//step 2 done 
