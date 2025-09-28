8224445a0755c1a8e0df3767d75421bbe46cd509
Nikita Siniayev <ns21@usf.edu>
2025-09-28 14:10:31 -0400

    step 3 done

--- cc_5.js
+++ cc_5.js
@@ -1,6 +1,15 @@
 const employees = [
-    {name: 'Alex', hourlyRate: 15, hoursWorked: 25},
-    {name: 'Sam', hourlyRate: 40, hoursWorked: 40},
+    {name: 'Alex', hourlyRate: 15, hoursWorked: 35},
+    {name: 'Sam', hourlyRate: 40, hoursWorked: 47},
     {name: 'Jordan', hourlyRate: 25, hoursWorked: 30}
 ];
 //step 2 done 
+
+function calculateBasePay(rate, hours) {
+    if (hours >40) {
+        hours = 40;
+    }
+    return rate * hours;
+}
+console.log(calculateBasePay(15, 35))
+//step 3 done 
